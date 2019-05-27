0a7396abe865dea72576c9ade0706a9ee2d97594
Clemens Holzkorn <40698612+cholzkorn@users.noreply.github.com>
2019-05-27 12:46:28 +0200

    added ctree

--- r-for-data-science/trees.R
+++ r-for-data-science/trees.R
@@ -1,21 +1,27 @@
-# Plotting Classification Trees with the plot.rpart and rattle pckages
-
-library(rpart)				        # Popular decision tree algorithm
-library(rattle)					# Fancy tree plot
-library(rpart.plot)				# Enhanced tree plots
-library(RColorBrewer)				# Color selection for fancy tree plot
-library(party)					# Alternative decision tree algorithm
-library(partykit)				# Convert rpart object to BinaryTree
-library(caret)					# Just a data source for this script
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
 
 seatbelts <- Seatbelts
 seatbelts <- as.data.frame(seatbelts)
 unique(seatbelts$law)
 
 
-seatbelts_tree <- rpart(law ~ ., data=seatbelts)
+seatbelts_tree <- rpart(law ~ ., data=seatbelts, control = rpart.control(cp=0.05))
 plot(seatbelts_tree, uniform = TRUE, margin = 0.5)
 text(seatbelts_tree)
 
 prp(seatbelts_tree)
-fancyRpartPlot(seatbelts_tree)
+fancyRpartPlot(seatbelts_tree, type=2)
+
+seatbelts_tree # The split is made on the variable with the lowest p-value
+
+
+# ctree solution - shows p values
+library(partykit)
+
+seatbelts_ctree <- ctree(law~ ., data=seatbelts)
+seatbelts_ctree
+
+plot(seatbelts_ctree)
