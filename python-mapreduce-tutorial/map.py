1313b799725884cb2700e21cf3e71f870c79913d
cholzkorn <c.holzkorn@hotmail.com>
2019-09-03 19:33:04 +0200

    added small tutorial on python mapreduce

---
+++ python-mapreduce-tutorial/map.py
@@ -0,0 +1,17 @@
+# tutorial: http://scienceblogs.de/diaxs-rake/2012/11/18/big-data-1-einfaches-word-count-beispiel-in-python/?all=1
+
+import sys
+
+def mapper():
+    for line in sys.stdin:
+        # strip() deletes leading and trailing characters, in this case " "
+        # split() splits the words
+        pp = line.strip().split()
+        for p in pp:
+            print(p, 1)
+
+if __name__ == '__main__':
+    mapper()
+
+# go to console and cat input.txt|python map.py
+    # for Windows: type input.txt|python map.py
---
+++ python-mapreduce-tutorial/reduce.py
@@ -0,0 +1,18 @@
+import sys
+
+def reducer():
+    counter = {}
+    for line in sys.stdin:
+        pp = line.strip().split()
+        if len(pp):
+            counter[pp[0]] = counter.get(pp[0], 0) + int(pp[1])
+
+    for word, count in sorted(counter.items(), key=lambda x:x[1]):
+        print(word, count)
+
+if __name__ == '__main__':
+    reducer()
+
+# go to console and type:
+    # unix: cat input.txt | python map.py | sort | python reduce.py
+    # windows: type input.txt | python map.py | sort | python reduce.py
