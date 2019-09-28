71c8a27fb5278e30e63b5b994c945c842679ed40
cholzkorn <c.holzkorn@hotmail.com>
2019-09-28 13:31:41 +0200

    added argv snippet

--- python-useful-snippets/square.py
+++ python-useful-snippets/square.py
@@ -1,6 +1,14 @@
 import sys
 import numpy as np
 
-x = int(sys.argv[1])
-y = x * x
-print(str(y))
+def xx():
+    x = int(sys.argv[1]) # takes first arg passed
+    y = x * x
+    print(str(y))
+
+def main():
+    xx()
+
+# this protects code from being executed if
+# the program is imported, not executed
+if __name__ == '__main__': main()
